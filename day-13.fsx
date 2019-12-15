bd893380f92567a0f48d63eaf77198e8c98ff337
Baine Wedlock <bainewedlock@gmail.com>
2019-12-15 18:24:58 +0100

    Add project files.

---
+++ day-13.fsx
@@ -0,0 +1,1 @@
+﻿#load "input.fsx"
---
+++ input.fsx
@@ -0,0 +1,13 @@
+﻿module Input
+
+open System.IO
+
+
+let asList name =
+  [|__SOURCE_DIRECTORY__; "input"; name|]
+  |> Path.Combine
+  |> FileInfo
+  |> fun f -> File.ReadAllLines(f.FullName)
+  |> Array.toList
+
+
